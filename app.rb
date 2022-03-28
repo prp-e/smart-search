e8d0f39686a8dfe67146bb1d2281fd5d95085232
Muhammadreza Haghiri <haghiri75@gmail.com>
2022-03-28 14:25:25 +0430

    Now it works, haha

--- app.rb
+++ app.rb
@@ -7,11 +7,14 @@ get '/' do
 end
 
 post '/search' do
+
+    headers = {"user-agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:98.0) Gecko/20100101 Firefox/98.0"}
+
     @query = params[:query]
     @url = ENV['SEARP_API']
     @query_url = @url + @query
 
-    @results = HTTParty.get(@query_url)
+    @results = HTTParty.get(@query_url, :headers => headers)
     @results = JSON.parse(@results.body)
     @results = @results['organic_results']
 
